39f28266a18cd35b4f36a6c88dce6ffadd141bf0
ningbnii <296720094@qq.com>
2019-12-11 13:14:38 +0800

    update

--- www/controllers/animation.js
+++ www/controllers/animation.js
@@ -2,52 +2,13 @@
   var w = document.body.clientWidth;
   var h = document.body.clientHeight;
 
-  LInit(10, 'animation', w, h, main);
+  LInit(requestAnimationFrame, 'animation', w, h, main);
 
-  var backgroundLayer,player,player2;
-  var walkDown = true;
-  var i=0;
+  var backgroundLayer;
+
 
   function main(event) {
     initBackgroundLayer();
-    var loader = new LLoader();
-    loader.addEventListener(LEvent.COMPLETE,loadBitmapdata);
-    loader.load('img/player.png','bitmapData');
-  }
-
-  function loadBitmapdata(event) {
-    var backLayer = new LSprite();
-    backgroundLayer.addChild(backLayer);
-    var list = LGlobal.divideCoordinate(480,630,3,4);
-    var data = new LBitmapData(event.target,0,0,120,210);
-    player = new LAnimation(backLayer,data,list);
-
-    player.y = -player.bitmap.height;
-    player.x = (w-player.bitmap.width)/2;
-
-    backgroundLayer.addEventListener(LEvent.ENTER_FRAME,onEnterFrame);
-  }
-
-  function onEnterFrame(event) {
-    player.onframe();
-    if(walkDown){
-      if(player.y<h){
-        player.y+=10;
-      }
-    }
-    if(!walkDown){
-      player.y -=10;
-    }
-
-    if(player.y>=h){
-      walkDown = false;
-      player.setAction(1,0);
-    }
-    if(player.y<=-player.bitmap.height){
-      player.setAction(0,0);
-      walkDown = true;
-    }
-
 
   }
 
@@ -58,15 +19,9 @@
   }
 
 
-
-
   $scope.$on('$ionicView.leave', function() {
     backgroundLayer.removeAllChild();
-    backgroundLayer.removeAllEventListener();
-
+    backgroundLayer.die();
   })
 
-  $scope.goToIndex = function() {
-    $state.go('index')
-  }
 })
